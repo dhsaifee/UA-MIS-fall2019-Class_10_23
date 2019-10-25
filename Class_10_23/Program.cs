c5bbc9527aeb1eccd1ec552c042f667ac5b0ec58
dhsaifee <dhsaifee@cba.ua.edu>
2019-10-25 14:17:50 -0500

    Add project files.

---
+++ Class_10_23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class_10_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] myStudent = new Student[50];
+
+            myStudent[0] = new Student("Jackson");
+            myStudent[1] = new Student("Rachel");
+            myStudent[2] = new Student("Sarah");
+
+            myStudent[0].SetClassRank("Junior");
+            myStudent[1].SetClassRank("Junior");
+            myStudent[2].SetClassRank("Senior");
+
+            Console.WriteLine(myStudent[2]); // Calls the ToString method
+
+            Console.WriteLine(myStudent[0].CompareTo(myStudent[1]));
+
+        }
+    }
+}
---
+++ Class_10_23/Student.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_10_23
+{
+    class Student
+    {
+        private string name;
+        private string classRank;
+        private double gpa;
+        private static int count;
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+        public Student()
+        {
+
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetClassRank(string classRank)
+        {
+            this.classRank = classRank;
+        }
+
+        public string GetClassRank()
+        {
+            return classRank;
+        }
+
+        public void SetGPA(double gpa)
+        {
+            this.gpa = gpa;
+        }
+
+        public double GetGPA()
+        {
+            return gpa;
+        }
+
+        public static void SetCount(int count)
+        {
+            Student.count = count;
+        }
+
+        public static int GetCount()
+        {
+            return count;
+        }
+
+        public static void IncCount()
+        {
+            count++;
+        }
+
+
+        public override string ToString()
+        {
+            return name + "\t" + classRank;
+        }
+
+        public int CompareTo(Student myStudent)
+        {
+            return this.name.CompareTo(myStudent.GetName());
+        }
+    }
+}
